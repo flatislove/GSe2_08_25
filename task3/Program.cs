e59420df60388873d69f6fb1c92ccd7b5f60da44
flvtisv <flatislove@gmail.com>
2022-08-26 00:01:09 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+int number = 0;
+while (true)
+{
+    Console.WriteLine("Введите число");
+    if (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+    else break;
+}
+if (number >= 0)
+{
+    for (int i = 1; i <= number; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + " * ");
+    }
+}
+else
+{
+    for (int i = 1; i >= number; i--)
+    {
+        Console.Write(Math.Pow(i, 3) + " * ");
+    }
+}
