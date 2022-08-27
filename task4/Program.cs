d1ed20b35606777874332eb0070c917fa0b563ef
flvtisv <flatislove@gmail.com>
2022-08-27 16:27:04 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int AreaInteger(int radius)
+{
+    return Convert.ToInt32(Math.Round(Math.PI * Math.Pow(radius, 2)));
+}
+int MaxDigit(int number)
+{
+    int[] array = number.ToString().ToCharArray().Select(x => int.Parse(x.ToString())).ToArray();
+    int max = array[0];
+    for (int i = 0; i < number.ToString().Length; i++)
+    {
+        max = Math.Max(max, array[i]);
+    }
+    return max;
+}
+
+Console.Clear();
+int radius = 0;
+while (true)
+{
+    Console.WriteLine("Введите радиус круга");
+    if (!int.TryParse(Console.ReadLine(), out radius) || radius < 0)
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+    else break;
+}
+
+Console.WriteLine(MaxDigit(AreaInteger(radius)));
